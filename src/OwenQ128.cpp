07f92d41abcf5795f8135b999cb8a9ba246fc1c1
stla <laurent_step@yahoo.fr>
2017-10-22 15:07:58 +0200

    vecOwenQ1

--- src/OwenQ128.cpp
+++ src/OwenQ128.cpp
@@ -19,23 +19,24 @@ double pt_boost(double q, double nu, double delta){
 NumericVector vecstudent(double q, int nu, NumericVector delta){
   const double a = R::sign(q)*sqrt(q*q/nu);
   const double b = nu/(nu+q*q);
+  const double sB = sqrt(b);
   const int J = delta.size();
   if(nu==1){
-    NumericVector C = pnorm(-delta*sqrt(b));
+    NumericVector C = pnorm(-delta*sB);
     int i;
     for(i=0; i<J; i++){
-      C[i] += 2*sf::owens_t(delta[i] * sqrt(b), a);
+      C[i] += 2*sf::owens_t(delta[i] * sB, a);
     }
     return C;
   }
   std::vector<double> dsB(J); 
   int j;
   for(j=0; j<J; j++){
-    dsB[j] = delta[j] * sqrt(b);
+    dsB[j] = delta[j] * sB;
   }
   std::vector< std::vector<double> > M(nu-1, std::vector<double>(J));
   for(j=0; j<J ; j++){
-    M[0][j] = a * sqrt(b) * R::dnorm(dsB[j], 0.0, 1.0, 0) * R::pnorm(a*dsB[j], 0.0, 1.0, 1, 0);
+    M[0][j] = a * sB * R::dnorm(dsB[j], 0.0, 1.0, 0) * R::pnorm(a*dsB[j], 0.0, 1.0, 1, 0);
   }
   const double sqrt2pi = 2.506628274631000502415765284811;
   if(nu>2){
@@ -60,7 +61,7 @@ NumericVector vecstudent(double q, int nu, NumericVector delta){
   }
   int i;
   if(nu%2==1){
-    NumericVector C = pnorm(-delta*sqrt(b));
+    NumericVector C = pnorm(-delta*sB);
     int i;
     for(i=0; i<J; i++){
       C[i] += 2*sf::owens_t(dsB[i], a);
@@ -90,6 +91,131 @@ NumericVector vecstudent(double q, int nu, NumericVector delta){
   return out;
 }
 
+NumericVector isPositive(NumericVector x){
+  int n = x.size();
+  NumericVector out(n);
+  int i;
+  for(i=0; i<n; i++){
+    out[i] = x[i] >= 0;
+  }
+  return out;
+}
+NumericVector isPositive(NumericVector x);
+
+NumericVector vecOwenQ1(int nu, double t, NumericVector delta, NumericVector R){
+  const double a = R::sign(t)*sqrt(t*t/nu);
+  const double b = nu/(nu+t*t);
+  const double sB = sqrt(b);
+  double ab;
+  double asB;
+  if(fabs(t)>DBL_MAX){
+    ab = 0;
+    asB = R::sign(t);
+  }else{
+    ab = a*b;
+    asB = R::sign(t)*sqrt(t*t/(nu+t*t));
+  }
+  const int J = delta.size();
+  if(nu==1){
+    NumericVector C = pnorm(R) - isPositive(delta);
+    int i;
+    for(i=0; i<J; i++){
+      double C1 =
+        sf::owens_t(delta[i]*sB, a);
+      double C2 =
+        sf::owens_t(R[i], a-delta[i]/R[i]);
+      double C3 =
+        sf::owens_t(delta[i]*sB, (ab-R[i]/delta[i])/b);
+      C[i] += 2*(C1 - C2 - C3);
+    }
+    return C;
+  }
+  std::vector<double> dsB(J); 
+  std::vector<double> dnormdsB(J); 
+  std::vector<double> dabminusRoversB(J); 
+  std::vector<double> dnormR(J); 
+  int j;
+  for(j=0; j<J; j++){
+    dsB[j] = delta[j] * sB;
+    dnormdsB[j] = R::dnorm(dsB[j], 0.0, 1.0, 0);
+    dabminusRoversB[j] = (delta[j]*ab - R[j])/sB; 
+    dnormR[j] = R::dnorm(R[j], 0.0, 1.0, 0);
+  }
+  const int n = nu-1;
+  std::vector< std::vector<double> > M(n, std::vector<double>(J));
+  std::vector< std::vector<double> > H(n, std::vector<double>(J));
+  for(j=0; j<J; j++){
+    H[0][j] = -dnormR[j] * R::pnorm(a*R[j]-delta[j], 0.0, 1.0, 1, 0);
+    M[0][j] = asB * dnormdsB[j] * (R::pnorm(dsB[j]*a, 0.0, 1.0, 1, 0) - R::pnorm(dabminusRoversB[j], 0.0, 1.0, 1, 0));
+  }
+  if(nu >= 3){
+    for(j=0; j<J; j++){
+      H[1][j] = R[j] * H[0][j];
+      M[1][j] = delta[j]*ab*M[0,j] + ab * dnormdsB[j] * (R::dnorm(dsB[j]*a, 0.0, 1.0, 0) - R::dnorm(dabminusRoversB[j], 0.0, 1.0, 0));
+    }
+    if(nu >= 4){
+      std::vector<double> A(n);
+      std::vector< std::vector<double> > L(n-2, std::vector<double>(J));
+      A[0] = 1;
+      A[1] = 1;
+      for(j=0; j<J; j++){
+        L[0][j] = ab * R[j] * dnormR[j] * R::dnorm(a*R[j]-delta[j], 0.0, 1.0, 1)/2;
+      }
+      int k;
+      for(k=2; k<n; k++){
+        A[k] = 1.0/k/A[k-1];
+      }
+      if(nu >= 5){
+        for(k=1; k<n-2; k++){
+          for(j=0; j<J; j++){
+            L[k][j] = A[k+2] * R[j] * L[k-1][j];
+          }
+        }
+      }
+      for(k=2; k<n; k++){
+        for(j=0; j<J; j++){
+          H[k][j] = A[k] * R[j] * H[k-1][j];
+          M[k][j] = (k-1.0)/k * (A[k-2] * delta[j] * ab * M[k-1][j] + b*M[k-2][j]) - L[k-2][j];
+        }
+      }
+    }
+  }
+  if(nu % 2 == 0){
+    const double sqrt2pi = 2.506628274631000502415765284811;
+    int i;
+    std::vector<double> sum(J);
+    for(i=0; i<n; i+=2){
+      for(j=0; j<J; j++){
+        sum[j] += M[i][j]+H[i][j];
+      }
+    }
+    NumericVector out(J);
+    for(j=0; j<J; j++){
+      out[j] = R::pnorm(-delta[j], 0.0, 1.0, 1, 0) + sqrt2pi*sum[j];
+    }
+    return out;
+  }else{
+    std::vector<double> sum(J);
+    for(i=1; i<n; i+=2){
+      for(j=0; j<J; j++){
+        sum[j] += M[i][j]+H[i][j];
+      }
+    }
+    NumericVector out(J);
+    NumericVector C = pnorm(R) - isPositive(delta);
+    for(j=0; i<J; j++){
+      double C1 =
+        sf::owens_t(dsB[j], a);
+      double C2 =
+        sf::owens_t(R[j], a-delta[j]/R[j]);
+      double C3 =
+        sf::owens_t(dsB[j], (ab-R[j]/delta[j])/b);
+      C[j] += 2*(C1 - C2 - C3);
+      out[j] = C[j] + 2*sum[j];
+    }
+    return out;
+  }
+}
 
 mp::float128 pnorm_mp(mp::float128 x){
   const mp::float128 sqrt2 = 1.4142135623730950488016887242096980785696718753769Q;
